64b7ef5a2b2a1967206c1ba6e0f2d3978051266b
heliosalesjr <heliosalesjr@gmail.com>
2023-04-03 18:48:18 -0300

    first setup

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <div>
+        <h1>Drag Knights</h1>
+        <ul>
+            <li>
+                Cartinha Show
+            </li>
+
+            <li>
+
+            </li>
+            
+            <li>
+
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default HomeScreen
